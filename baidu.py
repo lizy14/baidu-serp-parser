f69ab8406a48007714ded5e8cb94432f9bf71bd3
Li Zhaoyang <lizy14@yeah.net>
2015-08-14 16:19:16 +0800

    uses regexp instead of xml.dom

--- baidu.py
+++ baidu.py
@@ -1,65 +1,69 @@
-import xml.dom.minidom
+import re
 import http.client
 import html
 import urllib.parse
 import sys
-def BaiduSERP(keyword):
+
+def getBaiduSERP(keyword):
 	conn = http.client.HTTPConnection("m.baidu.com")
 	try:
-		conn.request("GET", "/s?word="+keyword)
+		conn.request("GET","/s?word="+urllib.parse.quote(keyword))
 	except:
 		print('Connection Failed')
-		return
+		return ''
 		
 	resp = conn.getresponse()
-
+	
 	if resp.status==200:
-		
 		page = str(resp.read(), 'UTF-8')
-		
-		#xml规范化
-		page=page.replace('<?xml version="1.0" encoding="utf-8"?>','')
-		page=page.replace('<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">','')
-		page=page.replace('&#160;','')
-		page=page.replace('&nbsp;','')
-		
-		#去除附加格式
-		page=page.replace('<em>','')
-		page=page.replace('</em>','')
-		
-		dom=xml.dom.minidom.parseString(page).documentElement
-		divs=dom.getElementsByTagName('div')
-
-		def getInnerText(obj):
-			if obj.nodeType==obj.TEXT_NODE :
-				return html.unescape(obj.data)
-			ret=""
-			delim=" "
-			if obj.tagName=='style':
-				return ""		
-			if (obj.tagName=='em'):
-				delim=""
-			else:
-				delim=' '
-			for child in obj.childNodes:
-				ret=ret+getInnerText(child)
-			return ret+delim
-
-		for div in divs:
-			if div.getAttribute('class')=='resitem':
-				try:
-					print(getInnerText(div))
-				except:
-					pass
-				print()
-				
+		return page
 	else:
 		print('HTTP CODE '+resp.status+': '+resp.reason)
-	conn.close()
+		return ''
+	
+
+def parseItem(item):
+	
+	site = (re.search('<span class="site" ?>(.*?)</span>', item))
+	site = site.group(1) if site else ''
+	date = (re.search('<span class="date" ?>(.*?)</span>', item))
+	date = date.group(1) if date else ''
+	title = (re.search('<a[^>]*>(.*?)</a>', item))
+	title = title.group(1) if title else ''
+	excerpt = (re.search('<div class="abs">(.*?)<span class="site" ?>', item))
+	excerpt = excerpt.group(1) if excerpt else ''
+	item_ = {'title': title, 'excerpt': excerpt, 'site': site, 'date': date}
+	
+	return item_
+	
+	
+def parseBaiduSERP(page):
+		
+		results=[]
+		
+		try:
+			match = re.search('<div class="reswrap">(.+)</div><div class="pagenav" >',page)
+			items = match.group(1)
+			match = re.findall('<div class="resitem"[^>]*?>(.+?</div>)</div>',items)
+			for item in match:
+				results.append(parseItem(item))
+		except:
+			print('Parse Error')
+		
+		return results
+	
 def main():
+	
 	while True:
-		keyword = urllib.parse.quote(input('keyword>'))
-		BaiduSERP(keyword)
+		keyword = (input('keyword>'))
+		
+		results = parseBaiduSERP(getBaiduSERP(keyword))
+		for result in results:
+			print(result['title'])
+			print(result['excerpt'])
+			print(result['site']+' '+result['date'])
+			print()
+			
 		if '-1' in sys.argv:
 			break
 main()
